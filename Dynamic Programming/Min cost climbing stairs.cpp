fa5eed6ef1a57c000caf284c07f6d5a1b86ee808
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2023-01-27 02:25:50 +0530

    Create Min cost climbing stairs.cpp

---
+++ Dynamic Programming/Min cost climbing stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(vector<int>& cost, int n, vector <int>& dp){
+        //Base Case
+        if(n <= 1)  return cost[n];
+
+        //Already Satisfied Case
+        if(dp[n] != -1) return dp[n];
+
+        //Standard Case
+        int incl = solve(cost, n - 2, dp);
+        int excl = solve(cost, n - 1, dp);
+        dp[n] = cost[n] + min(incl, excl);
+        return dp[n];
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector <int> dp(n + 1, -1);
+        int ans = min(solve(cost, n - 2, dp), solve(cost, n - 1, dp));
+        return ans;
+    }
+};
