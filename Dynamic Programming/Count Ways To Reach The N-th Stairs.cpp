cd96121a84f65f7aff6a499f50a758e77cddbd2e
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2023-01-27 02:29:41 +0530

    Create Count Ways To Reach The N-th Stairs.cpp

---
+++ Dynamic Programming/Count Ways To Reach The N-th Stairs.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+#include <vector>
+#define MOD 1000000007
+using namespace std;
+
+int solve(int nStairs, vector <int>& dp){
+    //Base Case
+    if(nStairs == 0 || nStairs == 1)    return 1;
+
+    //Already Satisfied Case
+    if(dp[nStairs] != -1)   return dp[nStairs];
+
+    //Standard  Case
+    dp[nStairs] = (solve(nStairs - 1, dp) + solve(nStairs - 2, dp))%MOD;
+    return dp[nStairs];
+}
+
+int countDistinctWays(int nStairs) {
+    vector <int> dp (nStairs + 1, -1);
+    return solve(nStairs, dp);
+}
