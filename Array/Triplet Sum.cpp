39ca83468e8fc29378894be58ed60b8eba1c2291
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2023-02-05 16:32:24 +0530

    Create Triplet Sum.cpp

---
+++ Array/Triplet Sum.cpp
@@ -0,0 +1,21 @@
+//Link: https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+class Solution{
+    public:
+    bool find3Numbers(int A[], int n, int X)
+    {
+        sort(A, A + n);
+        for(int i = 0; i<n; i++){
+            int j = i + 1;
+            int k = n - 1;
+            while(j < k){
+                int sum = A[i] + A[j] + A[k];
+                if(sum == X)    return true;
+                else if(sum < X)    j++;
+                else k--;
+            }
+        }
+        return false;
+    }
+
+};
