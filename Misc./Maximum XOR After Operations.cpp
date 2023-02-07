90ecdc0ae968cb9cc5d5e0d0f25d0a81bf35ed04
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2023-02-07 15:35:03 +0530

    Create Maximum XOR After Operations.cpp

---
+++ Misc./Maximum XOR After Operations.cpp
@@ -0,0 +1,12 @@
+//Link: https://leetcode.com/problems/maximum-xor-after-operations/description/
+
+class Solution {
+public:
+    int maximumXOR(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i<nums.size(); i++){
+            ans = ans | nums[i];
+        }
+        return ans;
+    }
+};
