05c5c1d40c7694030fa59d7e663fd51ecececce5
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2023-02-20 16:59:40 +0530

    Create Remove Duplicates from Sorted List II.cpp

---
+++ Linked List/Remove Duplicates from Sorted List II.cpp
@@ -0,0 +1,23 @@
+//Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* prev = dummy;
+        
+    while(head != NULL){
+        if(head -> next != NULL && head -> val == head -> next -> val){
+            while(head -> next != NULL && head -> val == head -> next -> val){
+                head = head -> next;
+            }
+            prev -> next = head -> next;
+        }
+        else {
+            prev = prev -> next;
+        }
+        head = head -> next;
+    }
+        return dummy -> next;
+    }
+};
