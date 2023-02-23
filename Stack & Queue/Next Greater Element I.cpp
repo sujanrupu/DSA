002e97472723e95b4834f11b523d4f781ff9f183
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2023-02-24 01:54:01 +0530

    Update Next Greater Element I.cpp

--- Stack & Queue/Next Greater Element I.cpp
+++ Stack & Queue/Next Greater Element I.cpp
@@ -2,29 +2,22 @@
 
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2);
-    };
-    vector <int> Solution :: nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector <int> v(nums1.size(), 0);
-        int found1, flag, i, j;
-        int size1 = nums1.size();
-        int size2 = nums2.size();
-        for(i = 0; i<size1; ++i){
-            found1 = 0;
-            flag = 0;
-            for(j = 0; j<size2; ++j){
-                if(nums1[i] == nums2[j]){
-                    found1 = 1;
-                }
-                if(found1 == 1 && nums1[i] < nums2[j]){
-                    v[i] = nums2[j];
-                    flag = 1;
-                    break;
-                }
+        int size1 = nums1.size(), size2 = nums2.size(), i, j, count1, count2;
+        for(i = 0; i<size1; i++){
+            count1 = 0;
+            count2 = 0;
+            for(j = 0; j<size2; j++){
+                if(nums1[i] == nums2[j])    count1 = 1;
+            if(count1 == 1 && nums1[i] < nums2[j]){
+                v[i] = nums2[j];
+                count2 = 1;
+                break;
             }
-            if(flag == 0){
-                v[i] = -1;
             }
+            if(count2 == 0) v[i] = -1;
         }
         return v;
     }
+};
