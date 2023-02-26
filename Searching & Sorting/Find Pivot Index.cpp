32c3561390cbfc917f3a428487e43ba29e1c297e
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2023-02-26 17:21:08 +0530

    Create Find Pivot Index.cpp

---
+++ Searching & Sorting/Find Pivot Index.cpp
@@ -0,0 +1,16 @@
+//Link: https://leetcode.com/problems/find-pivot-index/
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0, temp = 0;
+        for(auto i: nums)  sum += i;
+
+        for(int i = 0; i<nums.size(); i++){
+            sum -= nums[i];
+            if(sum == temp) return i;
+            temp += nums[i];
+        }
+        return -1;
+    }
+};
