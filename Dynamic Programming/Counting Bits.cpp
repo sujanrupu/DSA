adb39f598ebf11b6caf05cbde944d7e7d959faf2
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2023-03-05 01:10:11 +0530

    Create Counting Bits.cpp

---
+++ Dynamic Programming/Counting Bits.cpp
@@ -0,0 +1,16 @@
+//Link: https://leetcode.com/problems/counting-bits/description/
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int currMaxProduct = nums[0], currMinProduct = nums[0], maxProduct = nums[0];
+        for(int i=1; i<n; i++){
+            int temp = currMaxProduct;
+            currMaxProduct = max({nums[i], currMaxProduct*nums[i], currMinProduct*nums[i]});
+            currMinProduct = min({nums[i], temp*nums[i], currMinProduct*nums[i]});
+            maxProduct = max(maxProduct, currMaxProduct);
+        }
+        return maxProduct;
+    }
+};
